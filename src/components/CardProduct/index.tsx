21414e0f33eb53a2782c5a97b7ed4e964ea8ba07
evelynhelena <evelynhelena781@gmail.com>
2024-05-17 18:36:14 -0300

    criando home

--- src/components/CardProduct/index.tsx
+++ src/components/CardProduct/index.tsx
@@ -3,12 +3,36 @@ import { Minus, Plus, ShoppingCart } from 'phosphor-react'
 import './index.scss'
 import { useState } from 'react'
 import { ProductsTypesPropos } from '../../types/productService'
-import { ImageCoffee01 } from '../imgs/img01'
-import { ImageCoffee02 } from '../imgs/img02'
+import ImageCoffee01 from '../../assets/01-Image-coffee.svg'
+import ImageCoffee02 from '../../assets/02-Image-coffee.svg'
+import ImageCoffee03 from '../../assets/03-Image-coffee.svg'
+import ImageCoffee04 from '../../assets/04-Image-coffee.svg'
+import ImageCoffee05 from '../../assets/05-Image-coffee.svg'
+import ImageCoffee06 from '../../assets/06-Image-coffee.svg'
+import ImageCoffee07 from '../../assets/07-Image-coffee.svg'
+import ImageCoffee08 from '../../assets/08-Image-coffee.svg'
+import ImageCoffee09 from '../../assets/09-Image-coffee.svg'
+import ImageCoffee10 from '../../assets/10-Image-coffee.svg'
+import ImageCoffee11 from '../../assets/11-Image-coffee.svg'
+import ImageCoffee12 from '../../assets/12-Image-coffee.svg'
+import ImageCoffee13 from '../../assets/13-Image-coffee.svg'
+import ImageCoffee14 from '../../assets/14-Image-coffee.svg'
 
 const imagsList = {
-  '02-Image-coffee': <ImageCoffee02 />,
-  '01-Image-coffee': <ImageCoffee01 />,
+  '01-Image-coffee': ImageCoffee01,
+  '02-Image-coffee': ImageCoffee02,
+  '03-Image-coffee': ImageCoffee03,
+  '04-Image-coffee': ImageCoffee04,
+  '05-Image-coffee': ImageCoffee05,
+  '06-Image-coffee': ImageCoffee06,
+  '07-Image-coffee': ImageCoffee07,
+  '08-Image-coffee': ImageCoffee08,
+  '09-Image-coffee': ImageCoffee09,
+  '10-Image-coffee': ImageCoffee10,
+  '11-Image-coffee': ImageCoffee11,
+  '12-Image-coffee': ImageCoffee12,
+  '13-Image-coffee': ImageCoffee13,
+  '14-Image-coffee': ImageCoffee14,
 }
 
 export type ImagsListProps = keyof typeof imagsList
@@ -29,10 +53,9 @@ export function CardProduct({
 }: CardProductProps) {
   const [countProduct, setCountProduct] = useState<number>(0)
 
-  console.log(imageName)
   return (
     <Box className="box-product">
-      <>{imagsList[imageName]}</>
+      <img src={imagsList[imageName]} alt={description}></img>
       <Box p="5" className="content-infos">
         <Flex className="label-coffee-type" gap="2">
           {types.map((t, index) => (
