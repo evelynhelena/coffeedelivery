3ddadd28644d5100e0735d2821de4ab994899546
evelynhelena <evelynhelena781@gmail.com>
2024-05-17 19:54:48 -0300

    Configurando servidor

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,11 +1,18 @@
 import Axios from 'axios'
 
-const teste = window.location.hostname
+const { hostname } = window.location
 
-console.log(teste)
+console.log(hostname)
+
+const url =
+  hostname === 'localhost'
+    ? 'http://localhost:3000'
+    : 'https://servercoffeedelivery.vercel.app/'
+
+console.log(url)
 
 const api = Axios.create({
-  baseURL: 'https://servercoffeedelivery.vercel.app',
+  baseURL: url,
   headers: {
     'Content-Type': 'application/json',
   },
