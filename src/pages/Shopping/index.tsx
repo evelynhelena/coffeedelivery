cb5cc5692283a8a045803916b924d68a778df1da
evelynhelena <evelynhelena781@gmail.com>
2024-06-10 20:03:15 -0300

    Wip: Creating shopping page

--- src/pages/Shopping/index.tsx
+++ src/pages/Shopping/index.tsx
@@ -1,70 +1,171 @@
 import { Box, Flex, Text, TextField } from '@radix-ui/themes'
 import { Header } from '../../components/Header'
 import './index.scss'
-import { MapPinLine } from 'phosphor-react'
+import {
+  Bank,
+  CreditCard,
+  CurrencyDollar,
+  MapPinLine,
+  Money,
+} from 'phosphor-react'
+import { useShoppingCart } from '../../hooks/useShoppingCart'
+import { useState } from 'react'
 
 export function Shopping() {
+  const [paymentTypeSelect, setPaymentTypeSelect] = useState<
+    'CREDIT' | 'DEBIT' | 'MONEY' | undefined
+  >()
+
+  const { shoppingCartData } = useShoppingCart()
+
+  console.log(shoppingCartData)
+
   return (
     <>
       <Header />
       <Box className="mw-1120 content-shopping">
-        <Text className="baloo-2-extra-bold subtitle" size="4">
-          Complete seu pedido
-        </Text>
+        <Flex gap="6">
+          <Box>
+            <Box flexGrow="1">
+              <Text className="baloo-2-extra-bold subtitle" size="4">
+                Complete seu pedido
+              </Text>
 
-        <Box className="box-adress">
-          <Flex direction="column" gap="3">
-            <Box>
-              <Flex direction="column" gap="2">
-                <Text>
-                  <MapPinLine size={20} />
-                  Endereço de Entrega
-                </Text>
-                <Text>Informe o endereço onde deseja receber seu pedido</Text>
+              <Box className="box-adress" mt="5">
+                <Flex direction="column" gap="3">
+                  <Box>
+                    <Flex gap="2">
+                      <Box>
+                        <MapPinLine className="color-map-icon" size={20} />
+                      </Box>
+                      <Box>
+                        <Flex direction="column">
+                          <Text className="title">Endereço de Entrega</Text>
+                          <Text className="subtitle-box">
+                            Informe o endereço onde deseja receber seu pedido
+                          </Text>
+                        </Flex>
+                      </Box>
+                    </Flex>
+                  </Box>
+                  <Box className="box-textField">
+                    <Flex direction="column" gap="5">
+                      <TextField.Root
+                        className="text-field"
+                        size="3"
+                        placeholder="CEP"
+                      />
+                      <TextField.Root
+                        className="text-field"
+                        size="3"
+                        placeholder="Rua"
+                      />
+
+                      <Flex gap="2">
+                        <TextField.Root
+                          className="smalltext-field text-field"
+                          size="3"
+                          placeholder="Número"
+                        />
+                        <TextField.Root
+                          className="big-text-field text-field"
+                          size="3"
+                          placeholder="Complemento"
+                        >
+                          <TextField.Slot
+                            side="right"
+                            className="slot-textfield"
+                          >
+                            Opcional
+                          </TextField.Slot>
+                        </TextField.Root>
+                      </Flex>
+
+                      <Flex gap="2" justify="between">
+                        <Box>
+                          <TextField.Root
+                            className="text-field"
+                            size="3"
+                            placeholder="Número"
+                          />
+                        </Box>
+                        <Box width="276px">
+                          <TextField.Root
+                            className="text-field"
+                            size="3"
+                            placeholder="Complemento"
+                          />
+                        </Box>
+                        <Box width="60px">
+                          <TextField.Root
+                            className="text-field"
+                            size="3"
+                            placeholder="UF"
+                          />
+                        </Box>
+                      </Flex>
+                    </Flex>
+                  </Box>
+                </Flex>
+              </Box>
+            </Box>
+
+            <Box className="box-payment-type" mt="5">
+              <Flex gap="2">
+                <Box>
+                  <CurrencyDollar className="color-icons" size={20} />
+                </Box>
+                <Box>
+                  <Flex direction="column">
+                    <Text className="title">Pagamento</Text>
+                    <Text className="subtitle-box">
+                      O pagamento é feito na entrega. Escolha a forma que deseja
+                      pagar
+                    </Text>
+                  </Flex>
+                </Box>
+              </Flex>
+              <Flex mt="5" justify="between">
+                <Box
+                  className={`btn-payment-type ${paymentTypeSelect === 'CREDIT' && 'selected'}`}
+                  onClick={() => setPaymentTypeSelect('CREDIT')}
+                >
+                  <Flex gap="2" align="center">
+                    <CreditCard className="color-icons" size={20} />
+                    <Text className="payment-type-text">Cartão de crédito</Text>
+                  </Flex>
+                </Box>
+                <Box
+                  className={`btn-payment-type ${paymentTypeSelect === 'DEBIT' && 'selected'}`}
+                  onClick={() => setPaymentTypeSelect('DEBIT')}
+                >
+                  <Flex gap="2" align="center">
+                    <Bank className="color-icons" size={20} />
+                    <Text className="payment-type-text">cartão de débito</Text>
+                  </Flex>
+                </Box>
+                <Box
+                  className={`btn-payment-type ${paymentTypeSelect === 'MONEY' && 'selected'}`}
+                  onClick={() => setPaymentTypeSelect('MONEY')}
+                >
+                  <Flex gap="2" align="center">
+                    <Money className="color-icons" size={20} />
+                    <Text className="payment-type-text">dinheiro</Text>
+                  </Flex>
+                </Box>
               </Flex>
             </Box>
-            <Box className="box-textField">
-              <Flex direction="column" gap="5">
-                <TextField.Root
-                  className="text-field"
-                  size="3"
-                  placeholder="CEP"
-                />
-                <TextField.Root
-                  className="text-field"
-                  size="3"
-                  placeholder="Rua"
-                />
+          </Box>
 
-                <Flex gap="2">
-                  <TextField.Root
-                    className="smalltext-field text-field"
-                    size="3"
-                    placeholder="Número"
-                  />
-                  <TextField.Root
-                    className="big-text-field text-field"
-                    size="3"
-                    placeholder="Complemento"
-                  />
-                </Flex>
-
-                <Flex gap="2">
-                  <TextField.Root
-                    className="smalltext-field text-field"
-                    size="3"
-                    placeholder="Número"
-                  />
-                  <TextField.Root
-                    className="big-text-field text-field"
-                    size="3"
-                    placeholder="Complemento"
-                  />
-                </Flex>
-              </Flex>
+          <Box flexGrow="0" width="448px">
+            <Text className="baloo-2-extra-bold subtitle" size="4">
+              Cafés selecionados
+            </Text>
+            <Box mt="5">
+              <h1>teste</h1>
             </Box>
-          </Flex>
-        </Box>
+          </Box>
+        </Flex>
       </Box>
     </>
   )
