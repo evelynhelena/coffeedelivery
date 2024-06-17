abcb8da14df6b69b0251b63ae37e38f58fe440d5
ehsantos1 <ehsantos1@topazevolutiom.com>
2024-06-17 12:39:49 -0300

    teste

--- src/pages/Shopping/index.tsx
+++ src/pages/Shopping/index.tsx
@@ -50,6 +50,8 @@ export function Shopping() {
     getProcentValue()
   }, [getProcentValue])
 
+  console.log('teste')
+
   const handleSearchCep = async (cep: string) => {
     if (cep.length === 8) {
       try {
